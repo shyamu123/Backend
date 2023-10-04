af0cbe526c1f74e7345a90820ca028323bf3375f
shyamu123 <shyamrabadiya1@gmail.com>
2023-10-04 18:50:37 +0530

    2 commit

---
+++ Sql/Student.sql
@@ -0,0 +1,11 @@
+create table student(
+    rollno int primary key,
+    name varchar(100)
+);
+
+insert into student(rollno,name)values
+(1,"Shyamu"),
+(2,"Shubham");
+
+insert into student(rollno,name)values
+(3,"Vraj");
