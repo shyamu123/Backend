48f8a1e71b0a631db2f257b3d7e2438b5cef90c4
shyamu123 <shyamrabadiya1@gmail.com>
2023-10-05 13:48:25 +0530

    6 commit

--- Sql/DATABASES/college/student.sql
+++ Sql/DATABASES/college/student.sql
@@ -14,6 +14,28 @@ insert into student(rollno,name,marks,grade,city) values
 (105,"emanuel","12","F","Delhi"),
 (106,"farah","82","B","Delhi");
 
+create table dept(
+    id int primary key,
+    name varchar(100)
+);
+
+insert into dept(id,name)values
+(101,"English"),
+(102,"It");
+
+create table teacher( 
+    id int primary key,
+    name varchar(100),
+    dept_id int,
+    foreign key (dept_id) references dept(id)
+    on update cascade
+    on delete cascade
+);
+
+insert into teacher(id,name,dept_id)values
+(101,"Adam",101),
+(102,"Eve",102);
+
 +--------+---------+-------+-------+--------+
 | rollno | name    | marks | grade | city   |
 +--------+---------+-------+-------+--------+
@@ -23,4 +45,4 @@ insert into student(rollno,name,marks,grade,city) values
 |    104 | dhruv   |    96 | A     | Delhi  |
 |    105 | emanuel |    12 | F     | Delhi  |
 |    106 | farah   |    82 | B     | Delhi  |
-+--------+---------+-------+-------+--------+
++--------+---------+-------+-------+--------+   
